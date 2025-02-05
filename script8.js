31264d419115c1c8824c3a4a926da0cb6a2ced70
Kaushik Tapaniya <kaushik7984.com>
2025-02-05 16:31:46 +0530

    merge array

--- script8.js
+++ script8.js
@@ -1,1 +1,13 @@
-kt;
+// Function to merge two arrays and sort the result
+function mergeArrays(arr1, arr2) {
+  let arr3 = arr1.concat(arr2);
+  //arr3.sort();
+  arr3.sort((a, b) => a - b);
+  return arr3;
+}
+
+let arr1 = [1, 3, 5, 4, 755];
+let arr2 = [2, 4, 6, 8];
+let arr3 = mergeArrays(arr1, arr2);
+
+console.log(arr3.join(" "));
